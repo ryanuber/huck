7a0b87fc54f2e06d59f9b8ecab768506dfacd119
Ryan Uber <ru@ryanuber.com>
2014-07-13 11:40:02 -0700

    Get rid of app class - just use a module method for simplicity.

--- lib/cloudalogue.rb
+++ lib/cloudalogue.rb
@@ -6,3 +6,14 @@ require 'cloudalogue/facter_generator'
 require 'cloudalogue/ohai_generator'
 require 'cloudalogue/submitter'
 require 'cloudalogue/sqs_submitter'
+
+module Cloudalogue
+
+  # Main method to run Cloudalogue and dump info
+  def self.run kwargs = {}
+    gen_name = Cloudalogue::getarg kwargs, :generator, nil
+    g = Generator::factory gen_name
+    g.dump
+  end
+
+end
