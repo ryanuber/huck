3e0b8d7b595b176c56640647aa06ab6708e3343a
Ryan Uber <ru@ryanuber.com>
2014-07-17 00:19:11 -0700

    Added some stubbed testing to the Huck.serve method

--- spec/huck/huck_spec.rb
+++ spec/huck/huck_spec.rb
@@ -14,7 +14,6 @@ describe 'running the client' do
 
   it 'should read a config file' do
     file = mktempfile :content => <<EOF
----
 sender: sqs
 generators:
   - basic
@@ -24,6 +23,29 @@ EOF
 
   it 'should raise if a non-string is generated' do
     expect { Huck.run { 0 } }.to raise_error
+  end
+
+end
+
+describe 'running the server' do
+  before(:each) do
+    receiver = double
+    expect(receiver).to receive(:accept).once
+    allow(Huck::Receiver).to receive(:factory).and_return(receiver)
+  end
+
+  it 'should accept config as a hash' do
+    config = {'receiver' => 'sqs', 'handlers' => ['echo']}
+    Huck.serve :config => config
+  end
+
+  it 'should read a config file' do
+    file = mktempfile :content => <<EOF
+receiver: sqs
+handlers:
+  - echo
+EOF
+    Huck.serve :config_file => file
   end
 
 end
